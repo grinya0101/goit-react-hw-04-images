2edf24cbee6ffa8663f4743238c7a536d77e11fa
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-30 14:38:00 +0300

    Modal Refactored

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,36 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types"
 import { createPortal } from "react-dom";
 const modalPortal = document.querySelector("#modal-root");
 
-export class Modal extends Component{
-    escapeHendler = () => {
-        this.props.onClick("");
-    }
-    overlayClickHendler = event => {
+export const Modal = ({path, onClick}) => {
+    const overlayClickHendler = event => {
         if(event.target === event.currentTarget){
-            this.props.onClick("");
+            onClick("");
         }
     }
-    componentDidMount(){
-        window.addEventListener("keydown", this.escapeHendler);
-    }
-    componentWillUnmount(){
-        window.removeEventListener("keydown", this.escapeHendler);
-    }
-    render(){
-        return(
-            createPortal(<div onClick={this.overlayClickHendler} className="Overlay">
-                <div className="Modal">
-                    <img src={this.props.path} alt="Big img"/>
-                </div>
-            </div>, modalPortal)
-        )
-    }
+    useEffect(() => {
+        const escapeHendler = () => {
+            onClick("");
+        }
+        window.addEventListener("keydown", escapeHendler);
+        return() => {
+            window.removeEventListener("keydown", escapeHendler);
+            
+        };
+    }, [onClick]);
+
+
+    return(
+        createPortal(<div onClick={overlayClickHendler} className="Overlay">
+            <div className="Modal">
+                <img src={path} alt="Big img"/>
+            </div>
+        </div>, modalPortal)
+    )
 }
 
 Modal.propTypes = {
-    Path: PropTypes.string,
+    path: PropTypes.string,
     onClick: PropTypes.func
 }
