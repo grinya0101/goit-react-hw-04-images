ddb5928df75546aab043a60bb19d2a5883ed24ae
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-30 14:20:26 +0300

    Button refactored

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+export const Button = ({onClick}) => {
+    const handlerClick = () => {
+        onClick();
+    }
+    return(
+        <button type="button" className="Button" onClick={handlerClick}>Load more</button>
+    )
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func
+}
